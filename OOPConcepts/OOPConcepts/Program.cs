d2138246e7f6d1c696e6f35ab3740719d284c2cd
Rich10 <Rich10@DESKTOP-KO18HFC>
2023-03-06 00:33:39 +0100

    finish the class day

---
+++ OOPConcepts/OOPConcepts/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOPConcepts;
+
+try
+{ 
+	Console.WriteLine(new Date(1874,11,14));
+	Console.WriteLine(new Date(2023,1,31));
+
+}
+catch (Exception error)
+{
+	Console.WriteLine($"Error: {error.Message}");
+}
